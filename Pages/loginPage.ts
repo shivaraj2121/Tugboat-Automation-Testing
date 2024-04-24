95c6959756c789092391519a2cdb8ef1c53f8639
shivaraj2121 <shivarajb525@gmail.com>
2024-04-24 14:50:10 +0530

    added file changes

---
+++ Pages/loginPage.ts
@@ -0,0 +1,17 @@
+import actionHelper from "../Util/actionHelper"
+import { locator } from "../Pages/loginLocator"
+export default  new class LoginPage 
+
+{
+    async openURL(page,url)
+    {
+        await actionHelper.openApplication(page,url)
+    }
+    async login(page,username,password)
+    {
+        await actionHelper.click(page,locator.loginLink);
+        await actionHelper.type(page,locator.UserName,username);
+        await actionHelper.type(page,locator.passwordField,password);
+        await actionHelper.click(page,locator.loginButton);
+    }
+}
